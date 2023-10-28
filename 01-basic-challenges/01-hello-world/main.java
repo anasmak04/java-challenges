ed0c80dfc88bef5a74d75e91fa348b4d09153603
anasmak04 <anasdev8@gmail.com>
2023-10-28 14:52:01 +0200

    Implement Hello World challenge

---
+++ 01-basic-challenges/01-hello-world/main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+    System.out.println(getHello());
+    }
+
+
+    private static String getHello(){
+        return "Hello,World !";
+    }
+}
---
+++ 01-basic-challenges/01-hello-world/readme.md
@@ -0,0 +1,4 @@
+# Instructions
+
+Write a function called `helloWorld` that returns a string of 'Hello World!'.
+
